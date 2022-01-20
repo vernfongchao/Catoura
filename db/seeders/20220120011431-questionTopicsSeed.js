62a97059420018901bcc907ef54d3d351dd6bf0a
vernfongchao <Vernchao93@gmail.com>
2022-01-19 17:25:04 -0800

    questions/:id fixed

---
+++ db/seeders/20220120011431-questionTopicsSeed.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkInsert('Question_Topics', [
+     {questionId: 1, topicId: 1, createdAt: new Date(), updatedAt: new Date()}
+   ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('Question_Topics', null, {});
+  }
+};
--- routes/question.js
+++ routes/question.js
@@ -3,37 +3,35 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const { login, logout } = require('../auth');
 const db = require('../db/models');
-const { User, Question,Comment,Topic} = db;
+const { User, Question,Answer,Topic} = db;
 const { csrfProtection, userValidators, loginValidators, asyncHandler } = require('./utils');
 
-router.get('/:id(\\d+)', asyncHandler(async(req,res)=>{
+router.get('/:id(\\d+)',csrfProtection, asyncHandler(async(req,res)=>{
     const questionId = parseInt(req.params.id,10)
-    const question = await Question.findbyPk(questionId)
-        // {
-        //     include: [
-        //         {
-        //             model: Topic
-        //         },
-        //         {
-        //             model: Comment,
-        //             include: {
-        //                 model: User
-        //             }
-        //         }
+    const questions = await Question.findByPk(questionId,
+        {
+            include: [
+                        {
+                            model: Topic
+                        },
+                        {
+                            model:Answer
+                        }
+            ]
+        })
+    console.log(questions)
+    // res.render('question-details',{question})
 
-        //     ]
-        // })
-    console.log(question)
-    res.render('question-details',{question})
-    // if(question){
-    //     res.render('question-details',{
-    //         title: question.title ,
-    //         question, 
-    //         csrfToken: req.csrfToken()
-    //     })
-    // } else {
-    //     res.send('There is an error')
-    // }
+    if(questions){
+        res.render('question-details',{
+            title: questions.title ,
+            questions, 
+            csrfToken: req.csrfToken()
+        })
+    } else {
+        res.send('There is an error')
+    }
+
 }))
 
 
