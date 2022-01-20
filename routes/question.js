207d3bf6b4fe329e73deab4bf0132804db348cb8
vernfongchao <Vernchao93@gmail.com>
2022-01-19 21:22:13 -0800

    get question form and post question done without style

--- routes/question.js
+++ routes/question.js
@@ -4,7 +4,46 @@ const { check, validationResult } = require('express-validator');
 const { login, logout } = require('../auth');
 const db = require('../db/models');
 const { User, Question,Answer,Topic} = db;
-const { csrfProtection, userValidators, loginValidators, asyncHandler } = require('./utils');
+const { csrfProtection, questionValidators,asyncHandler } = require('./utils');
+
+
+router.get('/')
+
+
+
+router.get('/new',csrfProtection,asyncHandler(async(req,res)=>{
+    res.render('question-create',{title:'Question Form', csrfToken: req.csrfToken()})
+}))
+
+router.post('/', questionValidators, csrfProtection,asyncHandler(async(req,res)=>{
+    const {userId} = req.session.auth
+    const {
+        title,
+        content
+    } = req.body;
+    
+    const question = Question.build({
+        title,
+        content,
+        userId
+    });
+    console.log(question)
+    const validatorErrors = validationResult(req);
+    if (validatorErrors.isEmpty()) {
+        await question.save();
+        res.redirect(`/questions/${question.id}`);
+    } else {
+        const errors = validatorErrors.array().map((error) => error.msg);
+        res.render('question-create', {
+            title: 'Question Form',
+            question,
+            errors,
+            csrfToken: req.csrfToken(),
+        });
+    }
+}))
+
+
 
 router.get('/:id(\\d+)',csrfProtection, asyncHandler(async(req,res)=>{
     const questionId = parseInt(req.params.id,10)
@@ -35,7 +74,6 @@ router.get('/:id(\\d+)',csrfProtection, asyncHandler(async(req,res)=>{
     } else {
         res.send('There is an error')
     }
-
 }));
 
 
